697c423e15ae202b300410a3d9f8bbb19a7cf6cf
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2020-06-21 11:53:51 +0200

    added umfang column in serie table
    * also added table css

--- src/main/groovy/abakusFx/SerieTableController.groovy
+++ src/main/groovy/abakusFx/SerieTableController.groovy
@@ -10,9 +10,12 @@ import javafx.collections.FXCollections
 import javafx.fxml.FXML
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
+import javafx.scene.control.cell.TextFieldTableCell
+import javafx.util.converter.LocalDateStringConverter
 import org.javamoney.moneta.Money
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Log4j2
 class SerieTableController {
@@ -21,14 +24,15 @@ class SerieTableController {
         ObjectProperty<LocalDate> monat
         ObjectProperty<Gruppe> gruppe
         ObjectProperty<Stufe> stufe
-        // add: Umfang
+        ObjectProperty<BigDecimal> umfang
         ObjectProperty<Money> kosten
 
-        static Kosten of(LocalDate monat, Gruppe gruppe, Stufe stufe, Money kosten) {
+        static Kosten of(LocalDate monat, Gruppe gruppe, Stufe stufe, BigDecimal umfang, Money kosten) {
             Kosten k = new Kosten()
             k.monat = new SimpleObjectProperty<>(monat)
             k.gruppe = new SimpleObjectProperty<>(gruppe)
             k.stufe = new SimpleObjectProperty<>(stufe)
+            k.umfang = new SimpleObjectProperty<>(umfang)
             k.kosten = new SimpleObjectProperty<>(kosten)
             return k
         }
@@ -44,6 +48,8 @@ class SerieTableController {
     @FXML
     private TableColumn<Kosten, Stufe> stufeCol
     @FXML
+    private TableColumn<Kosten, BigDecimal> umfangCol
+    @FXML
     private TableColumn<Kosten, Money> kostenCol
 
     private javafx.collections.ObservableList<Kosten> kosten = FXCollections.observableArrayList()
@@ -51,12 +57,16 @@ class SerieTableController {
     @FXML
     void initialize() {
         monatCol.setCellValueFactory(cellData -> cellData.getValue().getMonat())
+        monatCol.setCellFactory(TextFieldTableCell.forTableColumn(new LocalDateStringConverter(DateTimeFormatter.ofPattern("LLL yyyy"),
+                DateTimeFormatter.ofPattern("LLL yyyy"))))
+
         gruppeCol.setCellValueFactory(cellData -> cellData.getValue().getGruppe())
         stufeCol.setCellValueFactory(cellData -> cellData.getValue().getStufe())
+        umfangCol.setCellValueFactory(cellData -> cellData.getValue().getUmfang())
         kostenCol.setCellValueFactory(cellData -> cellData.getValue().getKosten())
 
         kosten.addAll(Kosten.of(LocalDate.now(),
-                Gruppe.E10, Stufe.drei, Money.of(BigDecimal.TEN, Constants.euros)))
+                Gruppe.E10, Stufe.drei, BigDecimal.valueOf(90), Money.of(BigDecimal.TEN, Constants.euros)))
         kostenTabelle.setItems(kosten)
     }
 
