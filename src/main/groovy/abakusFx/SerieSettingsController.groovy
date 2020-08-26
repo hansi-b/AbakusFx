61c697503c279a5611a62c415b5fcbbc0f700b65
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2020-08-26 09:49:18 +0200

    issue #2 save & reload selection state
    * added Model class as values Adapter
    * added jackson (de)serialization for series state
      * currently completely as user pref, will switch to file

--- src/main/groovy/abakusFx/SerieSettingsController.groovy
+++ src/main/groovy/abakusFx/SerieSettingsController.groovy
@@ -4,6 +4,14 @@ import abakus.Anstellung
 import abakus.Gruppe
 import abakus.Stelle
 import abakus.Stufe
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.core.Version
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import groovy.transform.Immutable
 import groovy.util.logging.Log4j2
 import javafx.beans.value.ChangeListener
 import javafx.fxml.FXML
@@ -11,9 +19,63 @@ import javafx.scene.control.*
 
 import java.time.LocalDate
 import java.time.YearMonth
+import java.util.prefs.Preferences
+
+class External {
+
+    static final ObjectMapper objectMapper = createObjectMapper()
+
+    static class MyCustomSerializer extends JsonSerializer<LocalDate> {
+        @Override
+        void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.toString())
+        }
+    }
+
+    static class MyCustomDeserializer extends JsonDeserializer<LocalDate> {
+        @Override
+        LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            LocalDate.parse(p.getValueAsString())
+        }
+    }
+
+    private static ObjectMapper createObjectMapper() {
+        ObjectMapper om = new ObjectMapper(new YAMLFactory())
+        SimpleModule testModule = new SimpleModule("MyModule", new Version(1, 0, 0, null));
+        testModule.addSerializer(LocalDate.class, new MyCustomSerializer())
+        testModule.addDeserializer(LocalDate.class, new MyCustomDeserializer())
+        om.registerModule(testModule)
+        om
+    }
+}
 
 @Log4j2
 class SerieSettingsController {
+
+    @Immutable
+    static class Model {
+
+        LocalDate von
+        LocalDate bis
+        Gruppe gruppe
+        Stufe stufe
+        Integer umfang
+        Boolean isWeiter
+        LocalDate seit
+        Integer umfangSeit
+
+        static Model of(SerieSettingsController ssc) {
+            new Model(
+                    von: ssc.von.getValue(),
+                    bis: ssc.bis.getValue(),
+                    gruppe: ssc.gruppe.getValue(),
+                    stufe: ssc.stufe.getValue(),
+                    umfang: ssc.umfang.getValue(),
+                    isWeiter: ssc.weiter.isSelected(),
+                    seit: ssc.seit.getValue(),
+                    umfangSeit: ssc.umfangSeit.getValue())
+        }
+    }
 
     @FXML
     DatePicker von
@@ -44,31 +106,60 @@ class SerieSettingsController {
     @FXML
     Spinner<Integer> umfangSeit
 
-
     @FXML
     void initialize() {
 
-        von.setValue(LocalDate.now())
-        bis.setValue(LocalDate.now().plusMonths(3))
-
         gruppe.getItems().setAll(Gruppe.values())
-        gruppe.getSelectionModel().select(0)
-
+        // set the first toggle true to have one true
         neuOderWeiter.getToggles().first().setSelected(true)
-
         stufe.getItems().setAll(Stufe.values())
-        stufe.getSelectionModel().select(0)
-
-        seit.setValue(LocalDate.now().minusMonths(6))
 
         [seitLabel, seit, umfangSeitLabel, umfangSeit].each {
             it.disableProperty().bind(weiter.selectedProperty().not())
         }
+
+        Preferences prefs = Preferences.userNodeForPackage(SerieSettingsController.class)
+        String serieSettings = prefs.get("seriesSettings", "")
+        Model model = serieSettings ? prefsModel(serieSettings) : defaultModel()
+
+        setState(model)
+    }
+
+    Model prefsModel(String yaml) {
+        External.objectMapper.readValue(yaml, Model.class)
+    }
+
+    Model defaultModel() {
+        new Model(
+                von: LocalDate.now(),
+                bis: LocalDate.now().plusMonths(3),
+                gruppe: Gruppe.E10,
+                stufe: Stufe.eins,
+                umfang: 100,
+                isWeiter: false,
+                seit: LocalDate.now().minusMonths(6),
+                umfangSeit: 100
+        )
+    }
+
+    void setState(Model model) {
+        von.setValue(model.von)
+        bis.setValue(model.bis)
+        gruppe.setValue(model.gruppe)
+        stufe.setValue(model.stufe)
+        umfang.getValueFactory().setValue(model.umfang)
+        weiter.setSelected(model.isWeiter)
+        seit.setValue(model.seit)
+        umfangSeit.getValueFactory().setValue(model.umfangSeit)
+    }
+
+    Model getState() {
+        Model.of(this)
     }
 
     public <T> void addChangeListener(ChangeListener<T> changeListener) {
         [von, bis, gruppe, stufe, umfang, seit, umfangSeit].each { it.valueProperty().addListener(changeListener) }
-        weiter.selectedProperty().addListener(changeListener)
+        weiter.selectedProperty().addListener(changeListener as ChangeListener<? super Boolean>)
     }
 
     Stelle getStelle() {
@@ -83,5 +174,14 @@ class SerieSettingsController {
     def getAnstellung() {
         def beginn = YearMonth.from(weiter.selectedProperty().value ? seit.value : von.value)
         Anstellung.of(beginn, stelle, YearMonth.from(bis.value))
+    }
+
+    def stop() {
+        log.info "Saving ..."
+        Preferences prefs = Preferences.userNodeForPackage(SerieSettingsController.class)
+        Model model = getState()
+
+        String modelYaml = External.objectMapper.writeValueAsString(model)
+        prefs.put("seriesSettings", modelYaml)
     }
 }
