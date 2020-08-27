fc5349c95864bcd4e94be840d1b1e945c78f11ba
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2020-08-27 21:19:37 +0200

    refactoring: extracted SerieSettingsController.Model
    * is now abakusFx.SeriesModel

--- src/main/groovy/abakusFx/SerieSettingsController.groovy
+++ src/main/groovy/abakusFx/SerieSettingsController.groovy
@@ -4,58 +4,17 @@ import abakus.Anstellung
 import abakus.Gruppe
 import abakus.Stelle
 import abakus.Stufe
-import groovy.transform.Immutable
 import groovy.util.logging.Log4j2
 import javafx.beans.value.ChangeListener
 import javafx.fxml.FXML
 import javafx.scene.control.*
 
-import java.time.LocalDate
 import java.time.YearMonth
 
 @Log4j2
 class SerieSettingsController {
 
     private static final String SERIES_MODEL_KEY = "seriesSettings"
-    
-    @Immutable
-    static class Model {
-
-        LocalDate von
-        LocalDate bis
-        Gruppe gruppe
-        Stufe stufe
-        Integer umfang
-        Boolean isWeiter
-        LocalDate seit
-        Integer umfangSeit
-
-        static Model of(SerieSettingsController ssc) {
-            new Model(
-                    von: ssc.von.getValue(),
-                    bis: ssc.bis.getValue(),
-                    gruppe: ssc.gruppe.getValue(),
-                    stufe: ssc.stufe.getValue(),
-                    umfang: ssc.umfang.getValue(),
-                    isWeiter: ssc.weiter.isSelected(),
-                    seit: ssc.seit.getValue(),
-                    umfangSeit: ssc.umfangSeit.getValue())
-        }
-
-
-        static Model fallback() {
-            new Model(
-                    von: LocalDate.now(),
-                    bis: LocalDate.now().plusMonths(3),
-                    gruppe: Gruppe.E10,
-                    stufe: Stufe.eins,
-                    umfang: 100,
-                    isWeiter: false,
-                    seit: LocalDate.now().minusMonths(6),
-                    umfangSeit: 100
-            )
-        }
-    }
 
     @FXML
     DatePicker von
@@ -101,12 +60,12 @@ class SerieSettingsController {
         setState(readModel())
     }
 
-    private static Model readModel() {
+    private static SeriesModel readModel() {
         String modelString = new ModelStore().get(SerieSettingsController.class, SERIES_MODEL_KEY)
-        return modelString ? new ModelMapper().fromString(modelString, Model.class) : Model.fallback()
+        return modelString ? new ModelMapper().fromString(modelString, SeriesModel.class) : SeriesModel.fallback()
     }
 
-    void setState(Model model) {
+    void setState(SeriesModel model) {
         von.setValue(model.von)
         bis.setValue(model.bis)
         gruppe.setValue(model.gruppe)
@@ -117,8 +76,8 @@ class SerieSettingsController {
         umfangSeit.getValueFactory().setValue(model.umfangSeit)
     }
 
-    Model getState() {
-        Model.of(this)
+    SeriesModel getState() {
+        SeriesModel.of(this)
     }
 
     public <T> void addChangeListener(ChangeListener<T> changeListener) {
