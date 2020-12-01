cd7f5274ab9c75f8b1589862c79592e6e8eac1ba
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2020-12-01 16:17:55 +0100

    minor refactoring
    * made dragging class inner static

--- src/main/java/abakusfx/DragSelectCellFactory.java
+++ src/main/java/abakusfx/DragSelectCellFactory.java
@@ -8,37 +8,39 @@ import javafx.util.Callback;
 
 /**
  * adapted from https://community.oracle.com/tech/developers/discussion/2621389/
+ *
+ * Allows for selection of cells by dragging the mouse over them.
  */
-class DragSelectCell<O, T> extends TableCell<O, T> {
-
-	private Function<T, String> formatter;
-
-	public DragSelectCell(Function<T, String> formatter) {
-		this.formatter = formatter;
-		setOnDragDetected(e -> {
-			startFullDrag();
-			getTableColumn().getTableView().getSelectionModel().select(getIndex(), getTableColumn());
-		});
-		setOnMouseDragEntered(e -> {
-			getTableColumn().getTableView().getSelectionModel().select(getIndex(), getTableColumn());
-		});
-	}
-
-	@Override
-	public void updateItem(final T item, final boolean empty) {
-		super.updateItem(item, empty);
-		String result;
-		if (empty)
-			result = null;
-		else if (formatter == null && item != null)
-			result = item.toString();
-		else
-			result = formatter.apply(item);
-		setText(result);
-	}
-}
-
 public class DragSelectCellFactory<O, T> implements Callback<TableColumn<O, T>, TableCell<O, T>> {
+
+	private static class DragSelectCell<O, T> extends TableCell<O, T> {
+
+		private final Function<T, String> formatter;
+
+		DragSelectCell(final Function<T, String> formatter) {
+			this.formatter = formatter;
+			setOnDragDetected(e -> {
+				startFullDrag();
+				getTableColumn().getTableView().getSelectionModel().select(getIndex(), getTableColumn());
+			});
+			setOnMouseDragEntered(e -> {
+				getTableColumn().getTableView().getSelectionModel().select(getIndex(), getTableColumn());
+			});
+		}
+
+		@Override
+		public void updateItem(final T item, final boolean empty) {
+			super.updateItem(item, empty);
+			String result;
+			if (empty)
+				result = null;
+			else if (formatter == null && item != null)
+				result = item.toString();
+			else
+				result = formatter.apply(item);
+			setText(result);
+		}
+	}
 
 	private Function<T, String> formatter;
 
@@ -46,7 +48,7 @@ public class DragSelectCellFactory<O, T> implements Callback<TableColumn<O, T>, 
 		this(null);
 	}
 
-	public DragSelectCellFactory(Function<T, String> formatter) {
+	public DragSelectCellFactory(final Function<T, String> formatter) {
 		this.formatter = formatter;
 	}
 
