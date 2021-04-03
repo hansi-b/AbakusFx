5fec105aa10ae3d58d5a461cca7dac2b87417f82
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2021-04-03 21:41:31 +0200

    issue #37 allow for Übersicht-sorting
    * at the same time, keep sum row at bottom of table
    * as bonus, style borders of sum row differently

--- src/main/java/abakusfx/ÜbersichtTableController.java
+++ src/main/java/abakusfx/ÜbersichtTableController.java
@@ -2,6 +2,7 @@ package abakusfx;
 
 import static abakus.Constants.euros;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -15,10 +16,12 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 
 public class ÜbersichtTableController {
@@ -51,6 +54,9 @@ public class ÜbersichtTableController {
 	@FXML
 	private TableColumn<KostenÜbersicht, Money> kostenCol;
 
+	private static final PseudoClass sumRowCss = PseudoClass.getPseudoClass("sum-row");
+	private KostenÜbersicht sumRow;
+
 	@FXML
 	void initialize() {
 
@@ -62,10 +68,38 @@ public class ÜbersichtTableController {
 		final DoubleBinding colsWidth = nameCol.widthProperty().multiply(1.05);
 		kostenCol.prefWidthProperty().bind(übersichtTabelle.widthProperty().subtract(colsWidth));
 
+		übersichtTabelle.setItems(FXCollections.observableArrayList());
 		übersichtTabelle.setPlaceholder(new Label("Keine Daten"));
 		übersichtTabelle.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-		übersichtTabelle.setItems(FXCollections.observableArrayList());
 		CsvCopyTable.setCsvCopy(übersichtTabelle);
+		setRowFactoryForSumRowStyle();
+		setSortPolicyForSumRowPosition();
+	}
+
+	private void setRowFactoryForSumRowStyle() {
+		übersichtTabelle.setRowFactory(tv -> new TableRow<KostenÜbersicht>() {
+			@Override
+			protected void updateItem(final KostenÜbersicht k, final boolean b) {
+				super.updateItem(k, b);
+				pseudoClassStateChanged(sumRowCss, sumRow != null && k == sumRow);
+			}
+		});
+	}
+
+	private void setSortPolicyForSumRowPosition() {
+		übersichtTabelle.sortPolicyProperty().set(tv -> {
+			final Comparator<KostenÜbersicht> cmp = (row1, row2) -> {
+				if (sumRow != null && row1 == sumRow)
+					return 1;
+				if (sumRow != null && row2 == sumRow)
+					return -1;
+
+				final Comparator<KostenÜbersicht> tvCmp = tv.getComparator();
+				return tvCmp == null ? 0 : tvCmp.compare(row1, row2);
+			};
+			FXCollections.sort(tv.getItems(), cmp);
+			return true;
+		});
 	}
 
 	void updateItems(final List<KostenTab> tabs) {
@@ -75,15 +109,18 @@ public class ÜbersichtTableController {
 		final boolean isDirty = tabs.stream().anyMatch(t -> t.summe().get() == null);
 		if (!isDirty && tabs.size() > 1) {
 			final Money summe = tabs.stream().map(k -> k.summe().get()).reduce(euros(0), Money::add);
-			if (summe.isGreaterThan(euros(0)))
-				übersichtTabelle.getItems().add(KostenÜbersicht.of("∑", summe));
+			if (summe.isGreaterThan(euros(0))) {
+				sumRow = KostenÜbersicht.of("∑", summe);
+				übersichtTabelle.getItems().add(sumRow);
+			} else
+				sumRow = null;
 		}
+		übersichtTabelle.sort();
 	}
 
 	private static <T> void initCol(final TableColumn<KostenÜbersicht, T> col,
 			final Function<KostenÜbersicht, ObservableValue<T>> cellValueFac, final Function<T, String> formatter) {
 		col.setCellValueFactory(cellData -> cellValueFac.apply(cellData.getValue()));
 		col.setCellFactory(new DragSelectCellFactory<>(formatter));
-		col.setSortable(false);
 	}
 }
