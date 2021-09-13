e46e9a53b15c4cf6a72b2c974640004ffdc4859a
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2021-09-13 16:10:34 +0200

    issue #40 fixed FX application
    * added indirection starter class to work around
      https://github.com/javafxports/openjdk-jfx/issues/236
    * added windows-specific jpackage build instructions

---
+++ src/main/java/abakusfx/AppStarter.java
@@ -0,0 +1,11 @@
+package abakusfx;
+
+/*
+ * Needed as a workaround for https://github.com/javafxports/openjdk-jfx/issues/236
+ */
+public class AppStarter {
+
+	public static void main(String[] args) {
+		App.main(args);
+	}
+}
