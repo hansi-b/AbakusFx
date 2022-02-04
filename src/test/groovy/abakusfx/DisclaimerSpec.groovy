ed3a30803d102742e76d1a7c4b13b1f9c98971a2
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-02-04 21:59:20 +0100

    added simple test for disclaimer
    * check dialog for acceptance

---
+++ src/test/groovy/abakusfx/DisclaimerSpec.groovy
@@ -0,0 +1,50 @@
+package abakusfx
+
+import static org.testfx.api.FxAssert.*
+import static org.testfx.matcher.base.NodeMatchers.*
+
+import org.testfx.api.FxToolkit
+import org.testfx.framework.spock.ApplicationSpec
+import org.testfx.matcher.control.TableViewMatchers
+
+import abakusfx.AppPrefs.PrefKeys
+import javafx.collections.ObservableList
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.Tab
+import javafx.scene.control.TabPane
+import javafx.scene.input.KeyCode
+import javafx.stage.Stage
+import javafx.stage.Window
+
+public class DisclaimerSpec extends AbstractAbakusSpec {
+
+	void initAppPrefs() {
+		prefs.get(PrefKeys._version) >> AppPrefs.currentVersion
+		prefs.get(PrefKeys.wasDisclaimerAccepted) >>> ["false", "true"]
+	}
+
+	def "starting without prior disclaimer acceptance shows disclaimer"() {
+
+		when:
+		def s = findFocusedStage()
+		then:
+		s.getTitle().contains("Nutzungsvereinbarung")
+
+		when:
+		click("Ja")
+		then:
+		!findFocusedStage().getTitle().contains("Nutzungsvereinbarung")
+	}
+
+	/**
+	 * @return the last focused stage
+	 */
+	def findFocusedStage() {
+		return Window.getWindows().stream()//
+				.map(w -> (w instanceof Stage) ? (Stage) w : null) //
+				.filter(w -> w != null && w.isFocused())
+				.reduce((a, b) -> b).orElse(null)
+	}
+}
