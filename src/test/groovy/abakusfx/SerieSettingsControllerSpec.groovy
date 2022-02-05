d565471365b1caab1c18ef9f4b581d72e2bfac86
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-02-05 11:40:45 +0100

    reactivated Spec
    * SerieSettingsControllerSpec now works for simple case

--- src/test/groovy/abakusfx/SerieSettingsControllerSpec.groovy
+++ src/test/groovy/abakusfx/SerieSettingsControllerSpec.groovy
@@ -1,56 +1,63 @@
 package abakusfx
 
-import javafx.fxml.FXMLLoader
-import javafx.scene.Parent
-import javafx.scene.Scene
-import javafx.stage.Stage
-import org.testfx.api.FxToolkit
-import org.testfx.framework.spock.ApplicationSpec
-import spock.lang.Ignore
-
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
-@Ignore
+import org.testfx.api.FxToolkit
+import org.testfx.framework.spock.ApplicationSpec
+
+import abakus.Gruppe
+import abakus.Stelle
+import abakus.Stufe
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+import spock.lang.Shared
+
 public class SerieSettingsControllerSpec extends ApplicationSpec {
+	@Shared formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
-    Parent root
-    SerieSettingsController controller
+	Parent root
+	SerieSettingsController controller
 
-    @Override
-    void init() throws Exception {
-        FxToolkit.registerStage { new Stage() }
-    }
+	@Override
+	void init() throws Exception {
+		FxToolkit.registerStage { new Stage() }
+	}
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        final FXMLLoader fxmlLoader = abakusfx.ResourceLoader.loader.getFxmlLoader("serieSettings.fxml")
-        root = fxmlLoader.load()
-        controller = (SerieSettingsController) fxmlLoader.getController()
+	@Override
+	public void start(Stage stage) throws Exception {
+		final FXMLLoader fxmlLoader = abakusfx.ResourceLoader.loader.getFxmlLoader("serieSettings.fxml")
+		root = fxmlLoader.load()
+		controller = (SerieSettingsController) fxmlLoader.getController()
 
-        Scene scene = new Scene(root)
-        stage.setScene(scene)
-        stage.show()
-    }
+		Scene scene = new Scene(root)
+		stage.setScene(scene)
+		stage.show()
+	}
 
-    def "simple Anstellung"() {
-        when:
-        setPickerDate('#von', '2020-11-01')
+	def "simple Anstellung"() {
+		when:
+		setPickerDate('#von', '2020-12-25')
+		clickOn('#gruppe')
+		clickOn('E13')
+		clickOn('#stufe')
+		clickOn('5')
 
-        then:
-        Thread.sleep(3000)
-        controller.anstellung.am(YearMonth.of(2020, 12)) == null
-    }
+		then:
+		controller.anstellung.am(YearMonth.of(2020, 12)) == Stelle.of(Gruppe.E13, Stufe.fünf)
+	}
 
-    def setPickerDate(pickerId, dateString) {
-        def formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-        def localDate = LocalDate.parse(dateString, formatter)
-        lookup(pickerId).query().setValue(localDate)
-    }
+	def setPickerDate(pickerId, dateString) {
 
-    @Override
-    void stop() throws Exception {
-        FxToolkit.hideStage()
-    }
+		def localDate = LocalDate.parse(dateString, formatter)
+		lookup(pickerId).query().setValue(localDate)
+	}
+
+	@Override
+	void stop() throws Exception {
+		FxToolkit.hideStage()
+	}
 }
